c703944a6ceec59afbdffeb7181007814cc0f153
Daniela Diaz <dany.diaz1919@gmail.com>
2023-11-10 18:39:00 -0300

    punto2

---
+++ MiWebApi/Models/Usuario.cs
@@ -0,0 +1,7 @@
+public class Usuario{
+    int id;
+    string nombreUsuario;
+
+    public int Id { get => id; set => id = value; }
+    public string NombreUsuario { get => nombreUsuario; set => nombreUsuario = value; }
+}
